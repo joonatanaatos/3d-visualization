5ae8921af0b3f70fd1b513bbb097c869b45bf01c
Joonatan Korpela <joonatan.korpela@aalto.fi>
2023-03-05 14:05:30 +0200

    Add tests for World and Player

--- src/test/scala/ExampleSpec.scala
+++
@@ -1,8 +0,0 @@
-import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.matchers.should.Matchers
-
-class ExampleSpec extends AnyFlatSpec with Matchers {
-  "true" should "be true" in {
-    true should be(true)
-  }
-}
---
+++ src/test/scala/LogicSpec.scala
@@ -0,0 +1,59 @@
+import logic.{CursorListener, EventListener, KeyListener, World}
+import org.joml.Vector3f
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.concurrent.Eventually
+import org.lwjgl.glfw.GLFW
+
+import java.io.ByteArrayInputStream
+import java.nio.charset.StandardCharsets
+
+class LogicSpec extends AnyFlatSpec with Matchers {
+  behavior of "World"
+
+  var listenerOption: Option[EventListener] = None
+  val world = new World(l => listenerOption = Option(l))
+
+  "World" should "add an event listener" in {
+    Eventually.eventually {
+      listenerOption.isDefined should be(true)
+    }
+  }
+
+  val listener: Object =
+    listenerOption.getOrElse(() => throw new RuntimeException("listener was not defined"))
+  val keyListener: KeyListener = listener.asInstanceOf[KeyListener]
+  val cursorListener: CursorListener = listener.asInstanceOf[CursorListener]
+
+  "Player" should "move on key press" in {
+    val originalPosition = world.player.getPosition
+    keyListener.onKeyPress(GLFW.GLFW_KEY_W, GLFW.GLFW_PRESS)
+    world.tick()
+    keyListener.onKeyPress(GLFW.GLFW_KEY_W, GLFW.GLFW_RELEASE)
+    val newPosition = world.player.getPosition
+    newPosition.sub(originalPosition).length() should not be 0
+  }
+
+  "Player" should "eventually be stopped by a wall" in {
+    keyListener.onKeyPress(GLFW.GLFW_KEY_W, GLFW.GLFW_PRESS)
+    var i = 0
+    var previousPosition = world.player.getPosition
+    var currentPosition = Vector3f()
+    def positionChange = Vector3f(currentPosition).sub(previousPosition).length()
+    while i < 10000 && positionChange != 0 do {
+      world.tick()
+      previousPosition = currentPosition
+      currentPosition = world.player.getPosition
+      i += 1
+    }
+    keyListener.onKeyPress(GLFW.GLFW_KEY_W, GLFW.GLFW_RELEASE)
+    positionChange should be(0)
+  }
+
+  "Player" should "rotate on cursor move" in {
+    val originalDirection = world.player.getDirection
+    cursorListener.onCursorMove((5, 5))
+    val newDirection = world.player.getDirection
+    newDirection should not be originalDirection
+  }
+}
